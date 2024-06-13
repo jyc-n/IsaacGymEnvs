114235654ebfa8a5a3f5f77863cf33d57f4f3a79
Jingyu Chen <chenjy@g.ucla.edu>
2024-06-13 13:42:42 -0700

    additional configs for gpu pipeline

--- isaacgymenvs/extensions/test_sims.py
+++ isaacgymenvs/extensions/test_sims.py
@@ -1,18 +1,22 @@
 from isaacgym import gymapi
 from isaacgym import gymutil
+from isaacgym import gymtorch
 
 gym = gymapi.acquire_gym()
 
 args = gymutil.parse_arguments()
 
+# Tensor API only supports PhysX
 sim_params = gymapi.SimParams()
-sim = gym.create_sim(
-    args.compute_device_id, args.graphics_device_id, gymapi.SIM_PHYSX, sim_params
-    # args.compute_device_id, args.graphics_device_id, gymapi.SIM_FLEX, sim_params
-)
-
 sim_params.up_axis = gymapi.UP_AXIS_Y
 sim_params.gravity = gymapi.Vec3(0, -9.8, 0)
+# Use GPU pipeline
+sim_params.use_gpu_pipeline = True
+sim_params.physx.use_gpu = True
+
+sim = gym.create_sim(
+    args.compute_device_id, args.graphics_device_id, gymapi.SIM_PHYSX, sim_params
+)
 
 # configure the ground plane
 plane_params = gymapi.PlaneParams()
@@ -61,10 +65,29 @@ for e in range(3):
     color = gymapi.Vec3(e * 0.2, 0, 0)
     gym.set_rigid_body_color(env, actor_handle, 0, gymapi.MESH_VISUAL_AND_COLLISION, color)
 
+# Initialize the internal data structures used by the tensor API
+gym.prepare_sim(sim)
+
+# Get root tensor descriptor
+_root_tensor = gym.acquire_actor_root_state_tensor(sim)
+# Slice root tensor for more convenient access
+root_tensor = gymtorch.wrap_tensor(_root_tensor)
+root_positions = root_tensor[:, 0:3]
+root_orientations = root_tensor[:, 3:7]
+root_linvels = root_tensor[:, 7:10]
+root_angvels = root_tensor[:, 10:13]
+
+# Save root tensor
+saved_root_tensor = root_tensor.clone()
+
+step = 0
 while not gym.query_viewer_has_closed(viewer):
     # step the physics
     gym.simulate(sim)
     gym.fetch_results(sim, True)
+
+    # refresh the state tensors
+    gym.refresh_actor_root_state_tensor(sim)
 
     # update the viewer
     gym.step_graphics(sim)
