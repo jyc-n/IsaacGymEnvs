9852c24e7323934a45c3fddb7ceb5ffdc83cc89f
jyc-n <chenjy@ucla.edu>
2024-06-19 12:41:30 -0700

    cartpole learning

--- isaacgymenvs/tasks/extensions/test_cartpole.py
+++ isaacgymenvs/tasks/extensions/test_cartpole.py
@@ -1,4 +1,5 @@
 import os
+import torch
 import numpy as np
 from isaacgym import gymtorch, gymapi, gymutil
 
@@ -43,6 +44,7 @@ class TestCartpole(VecTask):
         # global tensors for computing observations
         dof_state_tensor = self.gym.acquire_dof_state_tensor(self.sim)
         self.dof_state = gymtorch.wrap_tensor(dof_state_tensor)
+        # dof_state is (1024 x 2) -> view as (512 x 2 x 2)
         self.dof_pos = self.dof_state.view(self.num_envs, self.num_dof, 2)[..., 0]
         self.dof_vel = self.dof_state.view(self.num_envs, self.num_dof, 2)[..., 1]
 
@@ -68,6 +70,50 @@ class TestCartpole(VecTask):
             self.cfg["env"]["envSpacing"],
             int(np.sqrt(self.num_envs)),
         )
+
+    def pre_physics_step(self, actions):
+        # implement pre-physics simulation code here
+        #    - e.g. apply actions
+        pass
+
+    def post_physics_step(self):
+        # implement post-physics simulation code here
+        #    - e.g. compute reward, compute observations
+        self.progress_buf += 1
+
+        env_ids = self.reset_buf.nonzero(as_tuple=False).squeeze(-1)
+        if len(env_ids) > 0:
+            self.reset_idx(env_ids)
+
+        self._compute_observations()
+        self._compute_reward()
+
+    # reset if in a bad state
+    def reset_idx(self, env_ids):
+        positions = 0.2 * (
+            torch.rand((len(env_ids), self.num_dof), device=self.device) - 0.5
+        )
+        velocities = 0.5 * (
+            torch.rand((len(env_ids), self.num_dof), device=self.device) - 0.5
+        )
+
+        self.dof_pos[env_ids, :] = positions[:]
+        self.dof_vel[env_ids, :] = velocities[:]
+
+        env_ids_int32 = env_ids.to(dtype=torch.int32)
+        self.gym.set_dof_state_tensor_indexed(
+            self.sim,
+            gymtorch.unwrap_tensor(self.dof_state),
+            gymtorch.unwrap_tensor(env_ids_int32),
+            len(env_ids_int32),
+        )
+
+        self.reset_buf[env_ids] = 0
+        self.progress_buf[env_ids] = 0
+
+    # ----
+    # Helper functions
+    # ----
 
     # initialization helpers
     def _create_ground_plane(self):
@@ -109,7 +155,7 @@ class TestCartpole(VecTask):
         # default initial transform
         pose = gymapi.Transform()
         pose.p = gymapi.Vec3(0.0, 0.0, 2.0)
-        pose.r = gymapi.Quat(0.0, 0.0, 0.0, 1.0) # z-up, identity quaternion
+        pose.r = gymapi.Quat(0.0, 0.0, 0.0, 1.0)  # z-up, identity quaternion
 
         self.cartpole_handles = []
         self.envs = []
@@ -123,7 +169,9 @@ class TestCartpole(VecTask):
 
             dof_props = self.gym.get_actor_dof_properties(env_ptr, cartpole_handle)
             # drive mode enables effort control
-            dof_props["driveMode"][0] = gymapi.DOF_MODE_EFFORT # The DOF will respond to effort (force or torque) commands
+            dof_props["driveMode"][0] = (
+                gymapi.DOF_MODE_EFFORT
+            )  # The DOF will respond to effort (force or torque) commands
             dof_props["driveMode"][1] = gymapi.DOF_MODE_NONE
             dof_props["stiffness"][:] = 0.0
             dof_props["damping"][:] = 0.0
@@ -134,12 +182,85 @@ class TestCartpole(VecTask):
 
             print(f"create {i}")
 
-    def pre_physics_step(self, actions):
-        # implement pre-physics simulation code here
-        #    - e.g. apply actions
-        pass
+    # compute reward
+    def _compute_reward(self):
+        # retrieve observations
+        cart_pos = self.obs_buf[:, 0]
+        cart_vel = self.obs_buf[:, 1]
+        pole_angle = self.obs_buf[:, 2]
+        pole_vel = self.obs_buf[:, 3]
 
-    def post_physics_step(self):
-        # implement post-physics simulation code here
-        #    - e.g. compute reward, compute observations
-        pass
+        self.rew_buf[:], self.reset_buf[:] = compute_cartpole_reward(
+            pole_angle,
+            pole_vel,
+            cart_pos,
+            cart_vel,
+            self.reset_dist,
+            self.reset_buf,
+            self.progress_buf,
+            self.max_episode_length,
+        )
+
+    # update observation
+    def _compute_observations(self):
+        # this will retrieve from all envs
+        env_ids = np.arange(self.num_envs)
+
+        # update dof_state_tensor
+        self.gym.refresh_dof_state_tensor(self.sim)
+
+        # obs_buf is a (num_envs x num_obs) tensor
+        #       cart_pos, cart_vel, pole_angle, pole_vel
+        # squeeze doesn't have effect here, no extra dimensions
+        self.obs_buf[env_ids, 0] = self.dof_pos[env_ids, 0].squeeze()
+        self.obs_buf[env_ids, 1] = self.dof_vel[env_ids, 0].squeeze()
+        self.obs_buf[env_ids, 2] = self.dof_pos[env_ids, 1].squeeze()
+        self.obs_buf[env_ids, 3] = self.dof_vel[env_ids, 1].squeeze()
+
+        return self.obs_buf
+
+
+# need to jit compile so that the reward is computed on GPU
+@torch.jit.script
+def compute_cartpole_reward(
+    pole_angle,
+    pole_vel,
+    cart_pos,
+    cart_vel,
+    reset_dist,
+    reset_buf,
+    progress_buf,
+    max_episode_length,
+):
+    # type: (Tensor, Tensor, Tensor, Tensor, float, Tensor, Tensor, float) -> Tuple[Tensor, Tensor]
+
+    # reward: (num_envs)
+    # combo of angle deviated from upright, velocity of cart, and velocity of pole moving
+    reward = (
+        1.0
+        - pole_angle * pole_angle
+        - 0.01 * torch.abs(cart_vel)
+        - 0.005 * torch.abs(pole_vel)
+    )
+
+    # if too far or fall over, reset and penalize
+    reward = torch.where(
+        torch.abs(cart_pos) > reset_dist, torch.ones_like(reward) * -2.0, reward
+    )
+    reward = torch.where(
+        torch.abs(pole_angle) > np.pi / 2, torch.ones_like(reward) * -2.0, reward
+    )
+
+    reset = torch.where(
+        torch.abs(cart_pos) > reset_dist, torch.ones_like(reset_buf), reset_buf
+    )
+    reset = torch.where(
+        torch.abs(pole_angle) > np.pi / 2, torch.ones_like(reset_buf), reset
+    )
+
+    # reset episode if reaches max episode length
+    reset = torch.where(
+        progress_buf >= max_episode_length - 1, torch.ones_like(reset_buf), reset
+    )
+
+    return reward, reset
