24e50c91334dbc99ba74c9af182d3b51d422d09d
jyc-n <chenjy@ucla.edu>
2024-07-16 13:32:28 -0700

    added tool for plotting rewards

---
+++ isaacgymenvs/utils/plot_utils.py
@@ -0,0 +1,29 @@
+import sys
+import pandas as pd
+from matplotlib import pyplot as plt
+from pathlib import Path
+
+def combined_reward_plots(path, args):
+  data = []
+  for f in args:
+    filename = Path(path) / f
+    assert filename.is_file()
+
+    file = pd.read_csv(filename)
+    data.append(file)
+
+  result = pd.concat(data, axis=0)
+  result.sort_values(by=['Step'], inplace=True)
+  result.drop_duplicates(subset=['Step'], keep='first', inplace=True)
+  print(result)
+
+  result.plot(x='Step', y='Value', lw=0.5)
+  plt.xlabel('iter')
+  plt.ylabel('reward')
+  # plt.show()
+  plt.savefig('reward_plot.png', dpi=300)
+
+  return
+
+if __name__ == '__main__':
+  combined_reward_plots(sys.argv[1], sys.argv[2:])
